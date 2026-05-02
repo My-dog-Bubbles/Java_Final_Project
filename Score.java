940d5166709c081e019afefe4a8b49a9b69010ab
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-05-01 22:33:48 -0400

    Refactor Score class with documentation and constructors
    Updated the Score class to include detailed documentation and improved constructor parameters.

--- Score.java
+++ Score.java
@@ -2,30 +2,71 @@
 Name: Angel Carmichael
 Date: 04/29/26
 
-Description: This file scores the users score of how shitty their post was. 
+Description: This file scores the user’s post based on how “shitty” it is. 
+ */
+
+/**
+ * Represents a score evaluation for a user's post.
+ * This class stores the username and post content and is intended to be
+ * used for calculating or displaying a "shittiness" score in the game.
  */
 public class Score {
-    // how are we going to test the shittiness of a post. 
 
-    //
-    private String username = "";
-    private String post = "";
+    private String username;
+    private String post;
 
+    /**
+     * Default constructor that initializes fields with empty values.
+     */
     public Score() {
         this.post = "";
         this.username = "";
     }
 
-    public Score(String post, String username) {
-        this.post = post;
+    /**
+     * Constructs a Score object with a specified username and post.
+     *
+     * @param username the username associated with the score
+     * @param post the post content being evaluated
+     */
+    public Score(String username, String post) {
         this.username = username;
+        this.post = post;
     }
 
+    /**
+     * Gets the username associated with this score.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets the post content being evaluated.
+     *
+     * @return the post text
+     */
     public String getPost() {
         return post;
+    }
+
+    /**
+     * Sets the username associated with this score.
+     *
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Sets the post content to be evaluated.
+     *
+     * @param post the post text to set
+     */
+    public void setPost(String post) {
+        this.post = post;
     }
 }
